80f5ba7c51b6ad6611825e26d6219399377492ca
Мелисжан Тилибаев <metilibaev@edu.hse.ru>
2020-09-18 01:59:22 +0500

    Code

---
+++ Gavno/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Gavno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Введите число");
+            if (!double.TryParse(Console.ReadLine(), out a) || a < 999 || a > 9999)
+            {
+                Console.WriteLine("error");
+
+            }
+            else 
+            {
+                Console.WriteLine(a.ToString().Reverse().ToArray());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ffgffg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ffgffg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
