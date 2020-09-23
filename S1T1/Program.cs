1799fa7271ede2b0ed6756f548ba0f07c7ec3795
Мелисжан Тилибаев <metilibaev@edu.hse.ru>
2020-09-23 18:36:14 +0500

    Task01

---
+++ S1T1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace S1T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = Console.ReadLine();
+            const string b = "*";
+            for (var i = 0; i < a.Length; i++)
+            {
+                Console.Write(b);
+            }
+
+        }
+    }
+}
