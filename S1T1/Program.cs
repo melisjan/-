55e23ce1a743ebe117d57d8b5e17e894a406ce80
Мелисжан Тилибаев <metilibaev@edu.hse.ru>
2020-09-23 19:43:50 +0500

    S1T2

---
+++ S1T1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S1T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
